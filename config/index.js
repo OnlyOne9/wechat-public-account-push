b832c5da4fada7e2a744390d8283f8647f4522b1
OnlyOne9 <46412023+OnlyOne9@users.noreply.github.com>
2022-09-23 23:51:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "小赵同学",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: ["ofnMj52_a83k1Aqn59un98tQh0-8","ofnMj52bMlAKasWZ0lRhYkvxxkDY"],
+      id: "ofnMj52_a83k1Aqn59un98tQh0-8",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "G81oaMZEdocV5IpWANHAfwKmkHS2gIv_wBtOOEEXE1k",
       // 所在省份
