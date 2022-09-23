e214954360cbf5516ac27e8b07ac836d887acc50
OnlyOne9 <46412023+OnlyOne9@users.noreply.github.com>
2022-09-24 02:29:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -96,7 +96,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ofnMj52_a83k1Aqn59un98tQh0-8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "8NdttM9hIoe79fAnm3EALGNxPbqokGuZqltWihSKX4E",
+      useTemplateId: "RaCEhh4gCRQ-snTAZzUU6yZvirEG4uVJ4YhfA6exkBg",
       // 所在省份
       province: "上海",
       // 所在城市
