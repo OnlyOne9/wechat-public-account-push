bd7085c998419efb29f3e9424222a9490872989b
OnlyOne9 <46412023+OnlyOne9@users.noreply.github.com>
2022-09-24 02:20:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,9 +62,9 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "宝贝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ofnMj52_a83k1Aqn59un98tQh0-8",
+      id: "ofnMj52bMlAKasWZ0lRhYkvxxkDY",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Jgpmu7kAdWFxxrHedSPQahUoZP1JJOvm869_aPJhDGE",
+      useTemplateId: "yC-WDbpbGMXgDt8h0DtzqJOyObpGoj8fvmM4lDq1-Y8",
       // 所在省份
       province: "新疆",
       // 所在城市
@@ -96,7 +96,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ofnMj52_a83k1Aqn59un98tQh0-8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Jgpmu7kAdWFxxrHedSPQahUoZP1JJOvm869_aPJhDGE",
+      useTemplateId: "yC-WDbpbGMXgDt8h0DtzqJOyObpGoj8fvmM4lDq1-Y8",
       // 所在省份
       province: "上海",
       // 所在城市
