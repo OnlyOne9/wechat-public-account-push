2b40fd225f3ffa0d59f754617e6762a0b0214696
OnlyOne9 <46412023+OnlyOne9@users.noreply.github.com>
2022-09-24 01:36:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ofnMj52_a83k1Aqn59un98tQh0-8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "wcNN7YXwhL840rkyRtV4IhE7AdEswMFHukX_-tfIfs0",
+      useTemplateId: "fsMD48y6SRUlGLAS_OP3n8vQJ9romHFOJyra4PM42GU",
       // 所在省份
       province: "新疆",
       // 所在城市
@@ -96,7 +96,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ofnMj52_a83k1Aqn59un98tQh0-8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "wcNN7YXwhL840rkyRtV4IhE7AdEswMFHukX_-tfIfs0",
+      useTemplateId: "fsMD48y6SRUlGLAS_OP3n8vQJ9romHFOJyra4PM42GU",
       // 所在省份
       province: "上海",
       // 所在城市
