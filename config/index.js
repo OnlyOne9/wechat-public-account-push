fd77447c4bc13ef1d95df5f1779e35614a96a1c3
OnlyOne9 <46412023+OnlyOne9@users.noreply.github.com>
2022-09-24 01:02:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -68,13 +68,13 @@ export const USER_CONFIG = {
       // 所在省份
       province: "新疆维吾尔自治区",
       // 所在城市
-      city: "塔城地区",
+      city: "沙湾市",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '10-02',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "",
+      openUrl: "https://s.weibo.com/top/summary?cate=realtimehot",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -106,7 +106,7 @@ export const USER_CONFIG = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "",
+      openUrl: "https://s.weibo.com/top/summary?cate=realtimehot",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
